4f499fa4e96ea4aede8d7833872e734cc87dae81
Rajesh Choudhary <rajuvert18@gmail.com>
2022-12-29 15:39:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById("btn");
+const emojiName = document.getElementById("name");
+
+
+const emoji = [];
+
+async function getEmoji() {
+    let response = await fetch("https://emoji-api.com/emojis?access_key=01601f449abdb353ef2a063a4733f48dedb891c5");
+
+    data = await response.json();
+
+
+    for (let i = 0; i < 1200; i++) {
+        emoji.push({
+            emojiNam: data[i].character,
+            emojiCode: data[i].unicodeName,
+            category: data[i].group
+
+        });
+    }
+}
+
+getEmoji();
+
+
+
+//01601f449abdb353ef2a063a4733f48dedb891c5
+btn.addEventListener("click", () => {
+    let randomNum = ((Math.random() * (emoji.length - 1))).toFixed(0);
+    btn.innerText = emoji[randomNum].emojiNam;
+    emojiName.innerText = emoji[randomNum].emojiCode;
+});
